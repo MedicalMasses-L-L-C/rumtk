df4b910cf0b0d76297e97d4aa06089bdbb18da5f
Luis M. Santos <lsantos@medicalmasses.com>
2026-01-05 17:50:57 -0500

    Began setting up the project to have a benchmark suite such that we can quickly track the performance characteristics of the project.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

---
+++ examples/benchmark/parse/setup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+#     rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
+#     This toolkit aims to be reliable, simple, performant, and standards compliant.
+#     Copyright (C) 2026  Luis M. Santos, M.D.
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+#
+#     rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
+#     This toolkit aims to be reliable, simple, performant, and standards compliant.
+#     Copyright (C) 2025  Luis M. Santos, M.D.
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+mkdir demo
+mkdir demo/echo_interface
+
+echo "Setting up Interface Chain"
+./target/debug/rumtk-v2-interface --port 55555 --local > demo/echo_interface/out.log &
+sleep 1
+./target/debug/rumtk-v2-interface --port 55556 --local | ./target/debug/rumtk-v2-interface --outbound --port 55555 --local &
+sleep 1
+
+echo "Pushing Message through PIPEs!"
+cat examples/sample_hl7.hl7 | ./target/debug/rumtk-v2-interface --outbound --local --port 55556
+
+sleep 1
+
+echo "Output"
+DIFF=$( diff <(jq -S . examples/sample_hl7.json) <(jq -S . demo/echo_interface/out.log) )
+
+echo "Clean up"
+pkill -i -e -f rumtk-v2-interface
+rm -r demo/echo_interface
+
+if [ "$DIFF" != "" ]; then
+    echo "Values mismatch!"
+    echo "Diff: $DIFF"
+    exit 69
+fi
