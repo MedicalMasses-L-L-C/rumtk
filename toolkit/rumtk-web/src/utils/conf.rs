576a54eff4b8b83ad7332bfd1ffe4ac43d60a27c
Luis M. Santos <lsantos@medicalmasses.com>
2026-01-11 15:57:20 -0500

    Reorganizing the configuration structure.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-web/src/utils/conf.rs
+++ toolkit/rumtk-web/src/utils/conf.rs
@@ -18,3 +18,65 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+use crate::utils::types::MMString;
+use axum::extract::State;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+
+///
+/// This is a core structure in a web project using the RUMTK framework. This structure contains
+/// a series of fields that represent the web app initial state or configuration. The idea is that
+/// the web app can come bundled with a JSON config file following this structure which we can load
+/// at runtime. The settings will dictate a few key project behaviors such as properly labeling
+/// some components with the company name or use the correct language text.
+///
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct AppConf {
+    pub title: MMString,
+    pub description: MMString,
+    pub lang: MMString,
+    pub theme: MMString,
+    pub custom_css: bool,
+    //pub opts: TextMap,
+}
+
+impl AppConf {
+    pub fn default() -> Self {
+        Self::new(
+            MMString::from(""),
+            MMString::from(""),
+            MMString::from("en"),
+            MMString::from(""),
+            false,
+        )
+    }
+
+    pub fn default_site(title: MMString, description: MMString, custom_css: bool) -> Self {
+        Self::new(
+            title,
+            description,
+            MMString::from("en"),
+            MMString::from(""),
+            custom_css,
+        )
+    }
+
+    pub fn new(
+        title: MMString,
+        description: MMString,
+        lang: MMString,
+        theme: MMString,
+        custom_css: bool,
+    ) -> Self {
+        Self {
+            title,
+            description,
+            lang,
+            theme,
+            custom_css,
+        }
+    }
+}
+
+pub type SharedAppConf = Arc<Mutex<AppConf>>;
+pub type RouterAppConf = State<Arc<Mutex<AppConf>>>;
--- toolkit/rumtk-web/src/utils/types.rs
+++ toolkit/rumtk-web/src/utils/types.rs
@@ -18,14 +18,13 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-use axum::extract::State;
+use super::conf::*;
 use axum::extract::{Path, Query};
 use axum::response::Html;
 use phf::{Map, OrderedMap};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::collections::HashMap;
 use std::pin::Pin;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 pub type MMString = String;
 pub type URLPath<'a, 'b> = &'a [&'b str];
@@ -37,57 +36,6 @@ pub type RenderedPageComponents = Vec<MMString>;
 /* Router Match Types */
 pub type RouterComponents = Path<Vec<MMString>>;
 pub type RouterParams = Query<HashMap<MMString, MMString>>;
-
-#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
-pub struct AppConf {
-    pub title: MMString,
-    pub description: MMString,
-    pub lang: MMString,
-    pub theme: MMString,
-    pub custom_css: bool,
-    //pub opts: TextMap,
-}
-
-impl AppConf {
-    pub fn default() -> Self {
-        Self::new(
-            MMString::from(""),
-            MMString::from(""),
-            MMString::from("en"),
-            MMString::from(""),
-            false,
-        )
-    }
-
-    pub fn default_site(title: MMString, description: MMString, custom_css: bool) -> Self {
-        Self::new(
-            title,
-            description,
-            MMString::from("en"),
-            MMString::from(""),
-            custom_css,
-        )
-    }
-
-    pub fn new(
-        title: MMString,
-        description: MMString,
-        lang: MMString,
-        theme: MMString,
-        custom_css: bool,
-    ) -> Self {
-        Self {
-            title,
-            description,
-            lang,
-            theme,
-            custom_css,
-        }
-    }
-}
-
-pub type SharedAppConf = Arc<Mutex<AppConf>>;
-pub type RouterAppConf = State<Arc<Mutex<AppConf>>>;
 
 /* Config Types */
 pub type ComponentFunction = fn(URLPath, URLParams, SharedAppConf) -> HTMLResult;
