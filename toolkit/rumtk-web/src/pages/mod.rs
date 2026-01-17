6a6bde9a1eb54f2fb3d78614aa3ed9320c1374b4
Luis M. Santos <lsantos@medicalmasses.com>
2026-01-17 18:36:25 -0500

    #20 Added unit test to verify if the problem is at the cache level Began fixing the default page lookup logic.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-web/src/pages/mod.rs
+++ toolkit/rumtk-web/src/pages/mod.rs
@@ -34,21 +34,83 @@ pub type UserPages<'a> = Vec<PageItem<'a>>;
 pub type PageCacheItem = LazyRUMCacheValue<PageFunction>;
 
 static mut PAGE_CACHE: PageCache = new_cache();
-static DEFAULT_PAGE: PageFunction = index::index;
+pub static DEFAULT_PAGE: PageFunction = index::index;
+pub const DEFAULT_PAGE_NAME: &str = "default";
 
-pub fn register_page(name: &str, component_fxn: PageFunction) {
+pub fn register_page(name: &str, component_fxn: PageFunction) -> PageCacheItem {
     let key = RUMString::from(name);
     let r = rumtk_cache_push!(&raw mut PAGE_CACHE, &key, &component_fxn);
     println!(
-        "Registered page {} => page function [{}] == registered function [{}]",
+        "Registered page {} => page function [{:?}] == registered function [{:?}]",
         name,
         &r.deref(),
         &component_fxn
     );
+    r
 }
 
+///
+/// Helper function for retrieving pages registered in the global cache using a string key!
+///
+/// ## Example
+///
+/// ### With Named Page
+///
+/// ```
+/// use rumtk_core::strings::rumtk_format;
+/// use rumtk_web::pages::{register_page, get_page};
+/// use rumtk_web::utils::{SharedAppConf, RenderedPageComponents};
+/// use rumtk_web::{rumtk_web_render_component};
+///
+/// pub fn index(app_state: SharedAppConf) -> RenderedPageComponents {
+///     let title_welcome = rumtk_web_render_component!("title", [("type", "welcome")], app_state.clone());
+///
+///     vec![
+///         title_welcome,
+///     ]
+/// }
+///
+/// let r = rumtk_format!("{:?}", &register_page("index", index));
+/// let p = rumtk_format!("{:?}", &get_page("index"));
+///
+///  assert_eq!(&r, &p, "{}", rumtk_format!("The registered page does not match the retrieved page!\nGot: {:?}\nExpected: {:?}", &r, &p));
+///
+/// ```
+///
+/// ### With Default Page
+///
+/// ```
+/// use rumtk_core::strings::rumtk_format;
+/// use rumtk_web::pages::{register_page, get_page, DEFAULT_PAGE};
+/// use rumtk_web::utils::{SharedAppConf, RenderedPageComponents};
+/// use rumtk_web::{rumtk_web_render_component};
+///
+/// pub fn index(app_state: SharedAppConf) -> RenderedPageComponents {
+///     let title_welcome = rumtk_web_render_component!("title", [("type", "welcome")], app_state.clone());
+///
+///     vec![
+///         title_welcome,
+///     ]
+/// }
+///
+/// let r = rumtk_format!("{:?}", &register_page("index", index));
+/// let p = rumtk_format!("{:?}", &get_page(""));
+/// let default = rumtk_format!("{:?}", &DEFAULT_PAGE);
+///
+///  assert_ne!(&default, &p, "{}", rumtk_format!("The default page matches the retrieved page!\nGot: {:?}\nExpected: {:?}", &r, &p));
+///  assert_eq!(&r, &p, "{}", rumtk_format!("The registered page does not match the retrieved page!\nGot: {:?}\nExpected: {:?}", &r, &p));
+///
+/// ```
+///
 pub fn get_page(name: &str) -> PageCacheItem {
-    rumtk_cache_get!(&raw mut PAGE_CACHE, &RUMString::from(name), &DEFAULT_PAGE)
+    let default_page = rumtk_cache_get!(
+        &raw mut PAGE_CACHE,
+        &RUMString::from(DEFAULT_PAGE_NAME),
+        &DEFAULT_PAGE
+    )
+    .deref()
+    .clone();
+    rumtk_cache_get!(&raw mut PAGE_CACHE, &RUMString::from(name), &default_page)
 }
 
 pub fn init_pages(user_components: &UserPages) {
