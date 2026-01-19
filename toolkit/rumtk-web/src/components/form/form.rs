af36086f27b274d1b5b2d1e2f8c61982a8131f84
Luis M. Santos <lsantos@medicalmasses.com>
2026-01-18 20:34:33 -0500

    #24 Added auto hiding progress logic switch so it is optional.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-web/src/components/form/form.rs
+++ toolkit/rumtk-web/src/components/form/form.rs
@@ -20,7 +20,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-use crate::defaults::{DEFAULT_NO_TEXT, PARAMS_ENDPOINT, SECTION_ENDPOINTS};
+use crate::defaults::{DEFAULT_NO_TEXT, PARAMS_ENDPOINT, PARAMS_TARGET, SECTION_ENDPOINTS};
 use crate::utils::defaults::{
     DEFAULT_TEXT_ITEM, PARAMS_CSS_CLASS, PARAMS_MODULE, PARAMS_TYPE, SECTION_MODULES,
 };
@@ -40,11 +40,13 @@ use askama::Template;
             htmx.on('#form-{{typ}}', 'htmx:xhr:progress', function(evt) {
               let progressValue = evt.detail.loaded/evt.detail.total * 100;
               let progressElement = htmx.find('#progress');
-              progressElement.style.display = 'inline-block';
 
+              {% if auto_hide_progress %}
+              progressElement.hidden = false;
               if (progressValue >= 100) {
-                 progressElement.style.display = 'none';
+                 progressElement.hidden = true;
               }
+              {% endif %}
 
               progressElement.setAttribute('value', progressValue);
             });
@@ -68,12 +70,14 @@ struct Form<'a> {
     elements: &'a [RUMString],
     css_class: RUMString,
     custom_css_enabled: bool,
+    auto_hide_progress: bool,
 }
 
 pub fn form(_path_components: URLPath, params: URLParams, state: SharedAppConf) -> HTMLResult {
     let typ = rumtk_web_get_text_item!(params, PARAMS_TYPE, DEFAULT_TEXT_ITEM);
     let module = rumtk_web_get_text_item!(params, PARAMS_MODULE, typ);
     let endpoint = rumtk_web_get_text_item!(params, PARAMS_ENDPOINT, typ);
+    let auto_hide_progress = rumtk_web_get_text_item!(params, PARAMS_TARGET, "progress_hidden");
     let css_class = rumtk_web_get_text_item!(params, PARAMS_CSS_CLASS, DEFAULT_TEXT_ITEM);
 
     let module_store = rumtk_web_get_conf!(state, SECTION_MODULES);
@@ -92,6 +96,7 @@ pub fn form(_path_components: URLPath, params: URLParams, state: SharedAppConf) 
         endpoint: RUMString::from(endpoint_url),
         elements: elements.iter().as_ref(),
         css_class: RUMString::from(css_class),
-        custom_css_enabled
+        custom_css_enabled,
+        auto_hide_progress: auto_hide_progress == "progress_hidden",
     })
 }
