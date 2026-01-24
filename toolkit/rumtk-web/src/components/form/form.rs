ba0b7b4ae01906c89f9644619acf4bbef1d0eacd
Luis M. Santos <lsantos@medicalmasses.com>
2026-01-24 11:16:14 -0500

    #24 combining title into form block for cleaner replacement upon post.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-web/src/components/form/form.rs
+++ toolkit/rumtk-web/src/components/form/form.rs
@@ -20,13 +20,16 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-use crate::defaults::{DEFAULT_NO_TEXT, PARAMS_ENDPOINT, PARAMS_TARGET, SECTION_ENDPOINTS};
+use crate::defaults::{
+    DEFAULT_NO_TEXT, PARAMS_ENDPOINT, PARAMS_TARGET, PARAMS_TITLE, SECTION_ENDPOINTS,
+};
 use crate::utils::defaults::{
     DEFAULT_TEXT_ITEM, PARAMS_CSS_CLASS, PARAMS_MODULE, PARAMS_TYPE, SECTION_MODULES,
 };
 use crate::utils::types::{HTMLResult, RUMString, SharedAppState, URLParams, URLPath};
 use crate::{
-    rumtk_web_get_conf, rumtk_web_get_form, rumtk_web_get_text_item, rumtk_web_render_html,
+    rumtk_web_get_conf, rumtk_web_get_form, rumtk_web_get_text_item, rumtk_web_render_component,
+    rumtk_web_render_html,
 };
 use askama::Template;
 
@@ -67,6 +70,7 @@ use askama::Template;
 )]
 struct Form<'a> {
     typ: RUMString,
+    title: RUMString,
     module: RUMString,
     endpoint: RUMString,
     elements: &'a [RUMString],
@@ -77,10 +81,16 @@ struct Form<'a> {
 
 pub fn form(_path_components: URLPath, params: URLParams, state: SharedAppState) -> HTMLResult {
     let typ = rumtk_web_get_text_item!(params, PARAMS_TYPE, DEFAULT_TEXT_ITEM);
+    let title = rumtk_web_get_text_item!(params, PARAMS_TITLE, DEFAULT_NO_TEXT);
     let module = rumtk_web_get_text_item!(params, PARAMS_MODULE, typ);
     let endpoint = rumtk_web_get_text_item!(params, PARAMS_ENDPOINT, typ);
     let auto_hide_progress = rumtk_web_get_text_item!(params, PARAMS_TARGET, "progress_hidden");
     let css_class = rumtk_web_get_text_item!(params, PARAMS_CSS_CLASS, DEFAULT_TEXT_ITEM);
+
+    let title_elem = match title.is_empty() {
+        true => RUMString::default(),
+        false => rumtk_web_render_component!("title", [("type", title)], state.clone()),
+    };
 
     let module_store = rumtk_web_get_conf!(state, SECTION_MODULES);
     let module_name = rumtk_web_get_text_item!(&module_store, module, DEFAULT_NO_TEXT);
@@ -94,6 +104,7 @@ pub fn form(_path_components: URLPath, params: URLParams, state: SharedAppState)
 
     rumtk_web_render_html!(Form {
         typ: RUMString::from(typ),
+        title: title_elem,
         module: RUMString::from(module_name),
         endpoint: RUMString::from(endpoint_url),
         elements: elements.iter().as_ref(),
