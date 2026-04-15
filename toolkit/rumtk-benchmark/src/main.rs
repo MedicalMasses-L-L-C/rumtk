1e8408d0e932c4a861728e92e09b8011cc2af426
Luis M. Santos <lsantos@medicalmasses.com>
2026-04-15 16:45:01 -0400

    chore(rumtk-benchmark) minor refactor to usage of rumtk-web due to new changes.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-benchmark/src/main.rs
+++ toolkit/rumtk-benchmark/src/main.rs
@@ -1,15 +1,20 @@
 mod forms;
 mod pages;
 
-use rumtk_web::rumtk_web_run_app;
+use rumtk_web::{
+    rumtk_web_register_app_components, rumtk_web_register_app_switches, rumtk_web_run_app,
+    AppComponents,
+};
 
 fn main() {
-    rumtk_web_run_app!(
+    let app_components = rumtk_web_register_app_components!(
         vec![
             ("index", pages::index::index),
             ("other", pages::other::other),
         ],
         vec![],
         vec![("upload", forms::upload::upload_form),]
-    )
+    );
+
+    rumtk_web_run_app!(app_components);
 }
