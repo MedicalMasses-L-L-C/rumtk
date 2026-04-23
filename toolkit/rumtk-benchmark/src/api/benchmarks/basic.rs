78fd24ed745c5e92d0f8610f6f6da69889eb5858
Luis M. Santos <lsantos@medicalmasses.com>
2026-04-23 10:18:06 -0400

    feat(rumtk-benchmark) small refactor of usage pattern of rumtk_web_get_pipeline macro after recent changes.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-benchmark/src/api/benchmarks/basic.rs
+++ toolkit/rumtk-benchmark/src/api/benchmarks/basic.rs
@@ -18,8 +18,8 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use rumtk_core::strings::{RUMArrayConversions, RUMString};
 use rumtk_core::rumtk_pipeline_quick_run_async;
+use rumtk_core::strings::{RUMArrayConversions, RUMString};
 use rumtk_web::defaults::PARAMS_ID;
 use rumtk_web::jobs::{JobResult, JobResultType};
 use rumtk_web::{rumtk_web_get_job_manager, rumtk_web_get_pipeline, rumtk_web_render_component, rumtk_web_render_page_contents};
@@ -28,7 +28,7 @@ use rumtk_web::{APIPath, FormData, HTMLResult, RUMWebData, SharedAppState};
 async fn basic_processor(form: FormData, state: SharedAppState) -> JobResult {
     match form.form.get("basic_choice") {
         Some(pipeline_name) => {
-            let pipeline = rumtk_web_get_pipeline!(state, "basic", pipeline_name);
+            let pipeline = rumtk_web_get_pipeline!(state).get_pipeline("basic", pipeline_name);
             let result = rumtk_pipeline_quick_run_async!(pipeline).await?;
 
             Ok(JobResultType::JSON(result.to_vec().to_rumstring()))
