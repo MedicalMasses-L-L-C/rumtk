0710d78bac59a0444ef3899b8c1cec367e5045da
Luis M. Santos <lsantos@medicalmasses.com>
2026-05-27 19:10:03 -0400

    feat(rumtk-arena) implemented the allocation and reallocation tests
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-arena/src/lib.rs
+++ toolkit/rumtk-arena/src/lib.rs
@@ -6,7 +6,27 @@ pub use arena::Arena;
 
 #[cfg(test)]
 mod tests {
+    use crate::Arena;
+
     #[test]
-    fn it_works() {
+    fn test_arena_simple_allocation() {
+        let arena = Arena::new();
+        let mut v = Vec::<usize, &Arena>::with_capacity_in(10, &arena);
+
+        v.push(10);
+        v.push(10);
+
+        assert_eq!(v, [10, 10], "Failed to allocate and fill a small vector!");
+    }
+
+    #[test]
+    fn test_arena_simple_reallocation() {
+        let arena = Arena::new();
+        let mut v = Vec::<usize, &Arena>::with_capacity_in(1, &arena);
+
+        v.push(10);
+        v.push(10);
+
+        assert_eq!(v, [10, 10], "Failed to reallocate and fill a small vector!");
     }
 }
