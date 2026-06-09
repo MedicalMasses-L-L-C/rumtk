d26395b51aec209dc036da2f32bd5cbf7d7264e8
Luis M. Santos <lsantos@medicalmasses.com>
2026-06-09 10:22:34 -0400

    feat(rumtk-v2) restored old parser process_message()
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-v2-parse/src/main.rs
+++ toolkit/rumtk-v2-parse/src/main.rs
@@ -59,11 +59,24 @@ pub struct RUMTKInterfaceArgs {
 fn process_message(args: &RUMTKInterfaceArgs) -> RUMResult<()> {
     let stdin_msg = rumtk_read_stdin!()?;
     if !stdin_msg.is_empty() {
-        let msg = rumtk_v2_parse_message!(stdin_msg)?;
+        let out_data = match rumtk_deserialize!(&stdin_msg.as_slice().to_string()?) {
+            Ok(msg) => {
+                let parsed_msg: V2Message = msg;
+                rumtk_v2_generate_message!(&parsed_msg)
+            }
+            Err(_) => {
+                let msg = rumtk_v2_parse_message!(stdin_msg)?;
 
-        if !args.quiet {
-            rumtk_write_stdout!(&rumtk_v2_generate_message!(&msg));
-        }
+                if !args.quiet {
+                    rumtk_serialize!(&msg)?
+                    //RUMString::default()
+                } else {
+                    RUMString::default()
+                }
+            }
+        };
+
+        rumtk_write_stdout!(&out_data);
     }
     Ok(())
 }
