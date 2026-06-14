312e24861b62a9d8ca1377a2c49784caef4455e3
Luis M. Santos <lsantos@medicalmasses.com>
2026-06-14 18:43:53 -0400

    feature(rumtk) added benchmark micro
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

---
+++ toolkit/rumtk-core/src/instrumentation.rs
@@ -0,0 +1,35 @@
+/*
+ *     rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
+ *     This toolkit aims to be reliable, simple, performant, and standards compliant.
+ *     Copyright (C) 2026  Luis M. Santos, M.D. <lsantos@medicalmasses.com>
+ *     Copyright (C) 2026  MedicalMasses L.L.C. <contact@medicalmasses.com>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#[macro_export]
+macro_rules! rumtk_benchmark_snippet {
+    ( $closure:expr ) => {{
+        use std::time::Instant;
+
+        let start = Instant::now();
+        let r = $closure();
+        let end = Instant::now();
+
+        let time = end - start;
+        let micros = time.as_micros();
+
+        (r, micros)
+    }};
+}
