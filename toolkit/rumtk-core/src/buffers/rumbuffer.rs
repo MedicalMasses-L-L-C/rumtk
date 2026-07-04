dc0abb8c89250b0b9e620b8752bfe60c40f3f4f3
Luis M. Santos <lsantos@medicalmasses.com>
2026-07-03 20:39:40 -0400

    feat(rumtk-core) defining our internal RUMBuffer type to be lighter than the bytes crate's buffer type.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-core/src/buffers/rumbuffer.rs
+++ toolkit/rumtk-core/src/buffers/rumbuffer.rs
@@ -17,35 +17,38 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-use rumtk_arena::rumtk_dune_nullptr;
-use std::sync::{Arc, RwLock};
+use crate::base::RUMVec;
+use std::sync::Arc;
 
-type RUMBufferInner = Arc<RwLock<Vec<u8>>>;
+type RUMBufferInner = Arc<RUMVec<u8>>;
 
 
-#[derive(Debug, PartialEq)]
+#[derive(Default, Debug, PartialEq, Clone)]
 pub struct RUMBuffer {
     data: RUMBufferInner,
-    drop: bool,
+    offset: usize,
+    end: usize,
 }
 
 impl RUMBuffer {
-    pub const fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            data: rumtk_dune_nullptr!(),
-            drop: false,
+            data: Arc::new(Vec::new()),
+            offset: 0,
+            end: 0,
         }
     }
 
-    pub fn split_to(&self, index: usize) -> Self {
+    pub fn split_to(&self, offset: usize) -> Self {
         Self {
-            data: &self.data[..index],
-            drop: false
+            data: self.data.clone(),
+            offset: self.end,
+            end: self.offset + offset,
         }
     }
 }
 
-trait AsSlice {
+pub trait AsSlice {
     type Item;
     fn as_slice(&self) -> &[Self::Item];
 }
@@ -54,12 +57,43 @@ impl AsSlice for RUMBuffer {
     type Item = u8;
 
     fn as_slice(&self) -> &[Self::Item] {
-        self.data
+        &self.data[self.offset..self.end]
     }
 }
 
 impl AsRef<[u8]> for RUMBuffer {
     fn as_ref(&self) -> &[u8] {
-        self.data
+        &self.data[self.offset..self.end]
+    }
+}
+
+
+/////////////////////// Conversions ////////////////////////////////
+impl From<RUMVec<u8>> for RUMBuffer {
+    fn from(data: RUMVec<u8>) -> Self {
+        let data_length = data.len();
+        Self {
+            data: Arc::new(data),
+            offset: 0,
+            end: data_length,
+        }
+    }
+}
+
+impl From<&[u8]> for RUMBuffer {
+    fn from(data: &[u8]) -> Self {
+        let owned_data: Vec<u8> = data.to_vec();
+        let data_length = owned_data.len();
+        Self {
+            data: Arc::new(owned_data),
+            offset: 0,
+            end: data_length,
+        }
+    }
+}
+
+impl From<&str> for RUMBuffer {
+    fn from(data: &str) -> Self {
+        Self::from(data.as_bytes())
     }
 }
