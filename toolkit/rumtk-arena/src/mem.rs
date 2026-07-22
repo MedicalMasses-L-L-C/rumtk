c944f1c2d1e1557eebfa409b1c77a35991e0abb7
Luis M. Santos <lsantos@medicalmasses.com>
2026-07-22 15:45:23 -0400

    feat(rumtk-arena) moved recent buffer manipulation modules to rumtk-arena from rumtk-core to keep these a bit closer and more tidy since they are related to buffer related work.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-arena/src/mem.rs
+++ toolkit/rumtk-arena/src/mem.rs
@@ -17,9 +17,29 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+/*
+ *     rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
+ *     This toolkit aims to be reliable, simple, performant, and standards compliant.
+ *     Copyright (C) 2026  Luis M. Santos, M.D. <lsantos@medicalmasses.com>
+ *     Copyright (C) 2026  MedicalMasses L.L.C. <contact@medicalmasses.com>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+use crate::base::RUMString;
 use crate::base::RUMVec;
 use crate::cpu::CPU_SIMD_64_SIZE;
-use crate::strings::RUMString;
 /////////////////If using MiMalloc//////////////////////////////
 #[cfg(all(feature = "mimalloc", feature = "default"))]
 use mimalloc::MiMalloc;
