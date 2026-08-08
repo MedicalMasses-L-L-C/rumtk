c5b88e304f86ccde480ae89898139674e606ce90
Luis M. Santos <lsantos@medicalmasses.com>
2026-08-07 20:02:51 -0400

    feat(rumtk-web) added api fuzz target
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-web/fuzz/fuzz_targets/fuzz_api.rs
+++ toolkit/rumtk-web/fuzz/fuzz_targets/fuzz_api.rs
@@ -1,7 +1,18 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use rumtk_core::buffers::buffer_to_str;
+
+use rumtk_web::*;
+use std::thread::spawn;
 
 fuzz_target!(|data: &[u8]| {
+    let app_thread = spawn(|| {
+        rumtk_web_run_app!()
+    });
+
     // fuzzed code goes here
+    if let Ok(str) = buffer_to_str(data) {
+        rumtk_web_sync_get("127.0.0.1:3000/{}".format(str)).unwrap()
+    }
 });
