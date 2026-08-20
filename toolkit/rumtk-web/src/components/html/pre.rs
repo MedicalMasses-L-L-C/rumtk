b862dc5b17eaebb0c6f0b5ca1ae08927cf362e00
Luis M. Santos <lsantos@medicalmasses.com>
2026-08-20 19:46:38 -0400

    fix(rumtk-web) license notice which got duplicated by IDE for some reason.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-web/src/components/html/pre.rs
+++ toolkit/rumtk-web/src/components/html/pre.rs
@@ -17,26 +17,6 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-/*
- *     rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
- *     This toolkit aims to be reliable, simple, performant, and standards compliant.
- *     Copyright (C) 2026  Luis M. Santos, M.D. <lsantos@medicalmasses.com>
- *     Copyright (C) 2026  MedicalMasses L.L.C. <contact@medicalmasses.com>
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 use crate::defaults::{DEFAULT_NO_TEXT, PARAMS_CONTENTS};
 use crate::utils::defaults::{DEFAULT_TEXT_ITEM, PARAMS_CSS_CLASS};
 use crate::utils::types::{SharedAppState, URLParams, URLPath};
