30cd10b5490c1fcc1bf655808d9f34bf15076fe5
Luis M. Santos <lsantos@medicalmasses.com>
2026-08-20 19:48:51 -0400

    fix(rumtk-web) license notice duplication
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-benchmark/src/utils/types/meta.rs
+++ toolkit/rumtk-benchmark/src/utils/types/meta.rs
@@ -17,37 +17,16 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-/*
- *     rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
- *     This toolkit aims to be reliable, simple, performant, and standards compliant.
- *     Copyright (C) 2026  Luis M. Santos, M.D. <lsantos@medicalmasses.com>
- *     Copyright (C) 2026  MedicalMasses L.L.C. <contact@medicalmasses.com>
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-use crate::utils::types::{RUMWebTemplate, ReportRawResults};
+use crate::utils::types::RUMWebTemplate;
 use rumtk_core::base::RUMResult;
 use rumtk_core::buffers::buffer_to_string;
 use rumtk_core::buffers::*;
 use rumtk_core::serde::{RUMDeJson, RUMSerJson};
-use rumtk_core::strings::{rumtk_format, RUMString, RUMStringConversions};
-use std::convert::{From, TryFrom};
-use std::env::consts;
+use rumtk_core::strings::RUMString;
+use std::convert::TryFrom;
 use std::fmt::Debug;
 
-pub type MetaData = (RUMBuffer,RUMBuffer,RUMBuffer);
+pub type MetaData = (RUMBuffer, RUMBuffer, RUMBuffer);
 
 #[derive(Default, Debug, RUMDeJson, RUMSerJson, RUMWebTemplate)]
 #[template(
