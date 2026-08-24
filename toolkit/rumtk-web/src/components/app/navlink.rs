2a71378bdbc85299511700a1b6a18bb1fdde53da
Luis M. Santos <lsantos@medicalmasses.com>
2026-08-23 23:34:21 -0400

    fix(rumtk-v2) ensured page name is capitalized.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

--- toolkit/rumtk-web/src/components/app/navlink.rs
+++ toolkit/rumtk-web/src/components/app/navlink.rs
@@ -1,24 +1,4 @@
 /*
- *     rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
- *     This toolkit aims to be reliable, simple, performant, and standards compliant.
- *     Copyright (C) 2026  Luis M. Santos, M.D. <lsantos@medicalmasses.com>
- *     Copyright (C) 2026  MedicalMasses L.L.C. <contact@medicalmasses.com>
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
  * rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
  * This toolkit aims to be reliable, simple, performant, and standards compliant.
  * Copyright (C) 2025  Luis M. Santos, M.D. <lsantos@medicalmasses.com>
@@ -55,7 +35,7 @@ struct NavItem {
         {% if custom_css_enabled %}
             <link href='/static/components/navlink.css' rel='stylesheet'>
         {% endif %}
-        <a class='undecorated navlink f18 navlink-{{css_class}}' href='{{target.url}}'>{{target.title}}</a>
+        <a class='undecorated navlink f18 navlink-{{css_class}}' href='{{target.url}}'>{{target.title|capitalize}}</a>
     ",
     ext = "html"
 )]
